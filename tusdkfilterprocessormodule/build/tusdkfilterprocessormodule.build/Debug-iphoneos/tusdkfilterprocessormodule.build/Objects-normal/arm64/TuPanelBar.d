2e98209e2acc1f8c15f6589da7432776f759aa30
lpc <liupengcheng@tusdk.com>
2021-05-27 11:17:24 +0800

    *release 4.0.0

---
+++ tusdkfilterprocessormodule/build/tusdkfilterprocessormodule.build/Debug-iphoneos/tusdkfilterprocessormodule.build/Objects-normal/arm64/TuPanelBar.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/liupengcheng/Documents/TuSDKWorkSpace/ios_sdk_thirdParty/Zogo_LiveDemo-tusdk-iOS/tusdkfilterprocessormodule/tusdkfilterprocessormodule/Views/TuBaseView/TuPanelBar.m \
+  /Users/liupengcheng/Documents/TuSDKWorkSpace/ios_sdk_thirdParty/Zogo_LiveDemo-tusdk-iOS/tusdkfilterprocessormodule/tusdkfilterprocessormodule/Views/TuBaseView/TuPanelBar.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap
---
+++ tusdkfilterprocessormodule/build/tusdkfilterprocessormodule.build/Debug-iphoneos/tusdkfilterprocessormodule.build/Objects-normal/arm64/TuParametersAdjustView.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/liupengcheng/Documents/TuSDKWorkSpace/ios_sdk_thirdParty/Zogo_LiveDemo-tusdk-iOS/tusdkfilterprocessormodule/tusdkfilterprocessormodule/Views/TuBaseView/TuParametersAdjustView.m \
+  /Users/liupengcheng/Documents/TuSDKWorkSpace/ios_sdk_thirdParty/Zogo_LiveDemo-tusdk-iOS/tusdkfilterprocessormodule/tusdkfilterprocessormodule/Views/TuBaseView/TuParametersAdjustView.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/liupengcheng/Documents/TuSDKWorkSpace/ios_sdk_thirdParty/Zogo_LiveDemo-tusdk-iOS/tusdkfilterprocessormodule/tusdkfilterprocessormodule/Views/TuBaseView/TuCustomSlider.h
